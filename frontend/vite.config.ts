b5e5326b2f1faac97eab7efc4962439318fde534
Hansaka Matharaarachchi <m.a.hansaka@gmail.com>
2025-02-26 22:30:01 +0530

    setup vitest for testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -5,4 +7,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./test.setup.ts",
+	},
 });
